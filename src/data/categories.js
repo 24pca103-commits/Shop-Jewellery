dff9efdc635dc07fb166556b1cdc1ab7241f15e8
kanmani-hk <kan930995@gmail.com>
2026-07-04 14:09:55 +0530

    Add emojis for categories to show icons in the sub-menu

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,16 +1,16 @@
 export const CATEGORIES = [
-  { id: 'rings', label: 'Rings', image: 'https://png.pngtree.com/thumb_back/fh260/background/20230903/pngtree-blue-wedding-ring-with-diamond-image_13188386.jpg', description: 'Solitaire, bands & cocktail rings', count: 248 },
-  { id: 'earrings', label: 'Earrings', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB2N4oTm4Yt-9Gpsh5eXeSeJtBrPfBWikmFm0b94H_vsG531Q7cK_Oc&s=10', description: 'Jhumkas, studs & chandeliers', count: 312 },
-  { id: 'necklace', label: 'Necklace', image: 'https://www.tarinika.com/cdn/shop/files/ANX2598X_01a493c8-5f90-42f3-a15b-d85a539390ad_2048x.jpg?v=1695649017', description: 'Chokers, long & layered sets', count: 186 },
-  { id: 'pendant', label: 'Pendant', image: 'https://rukminim2.flixcart.com/image/480/640/xif0q/pendant-locket/c/9/c/na-d-p-box-chain-r7-001-reewaziperial-original-imahhzfgvcxgkahr.jpeg?q=90', description: 'Gold & diamond pendants', count: 142 },
-  { id: 'bangles', label: 'Bangles', image: 'https://trendia.co/cdn/shop/files/55027835_3.jpg?v=1780295382', description: 'Plain, stone & designer bangles', count: 204 },
-  { id: 'bracelets', label: 'Bracelets', image: 'https://assets.ajio.com/medias/sys_master/root/20231004/a1sO/651cc705afa4cf41f5215c09/-473Wx593H-466663725-gold-MODEL.jpg', description: 'Tennis, chain & cuff bracelets', count: 98 },
-  { id: 'chains', label: 'Chains', image: 'https://rudrakshartjewellery.in/cdn/shop/products/IMG_1239_2800x.jpg?v=1754640443', description: 'Singapore, rope & box chains', count: 156 },
-  { id: 'mangalsutra', label: 'Mangalsutra', image: 'https://m.media-amazon.com/images/I/61dIJjaZHoL._AC_UY300_.jpg', description: 'Traditional & modern styles', count: 89 },
-  { id: 'nosepins', label: 'Nose Pin', image: 'https://shop.southindiajewels.com/wp-content/uploads/2022/06/Taditional-Design-Nose-Pin-1-scaled.jpg', description: 'Classic & diamond nose pins', count: 74 },
-  { id: 'bridal', label: 'Bridal', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTlkj32cWHHomMJD-NpuCgyps5ux_WwXijyneI7UiAXi6beNUV_BG7Hg&s=10', count: 168 },
-  { id: 'diamond', label: 'Diamond', image: 'https://cdn.orra.co.in/media/catalog/product/cache/10238651d5f95594b9023f998383bb67/o/s/osn21062_ose21062.jpg', count: 234 },
-  { id: 'platinum', label: 'Platinum', image: 'https://images.meesho.com/images/products/454524369/1dmnu_512.webp?width=512', description: 'Timeless platinum jewellery', count: 45 },
+  { id: 'rings', label: 'Rings', emoji: '💍', image: 'https://png.pngtree.com/thumb_back/fh260/background/20230903/pngtree-blue-wedding-ring-with-diamond-image_13188386.jpg', description: 'Solitaire, bands & cocktail rings', count: 248 },
+  { id: 'earrings', label: 'Earrings', emoji: '💎', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB2N4oTm4Yt-9Gpsh5eXeSeJtBrPfBWikmFm0b94H_vsG531Q7cK_Oc&s=10', description: 'Jhumkas, studs & chandeliers', count: 312 },
+  { id: 'necklace', label: 'Necklace', emoji: '📿', image: 'https://www.tarinika.com/cdn/shop/files/ANX2598X_01a493c8-5f90-42f3-a15b-d85a539390ad_2048x.jpg?v=1695649017', description: 'Chokers, long & layered sets', count: 186 },
+  { id: 'pendant', label: 'Pendant', emoji: '🧿', image: 'https://rukminim2.flixcart.com/image/480/640/xif0q/pendant-locket/c/9/c/na-d-p-box-chain-r7-001-reewaziperial-original-imahhzfgvcxgkahr.jpeg?q=90', description: 'Gold & diamond pendants', count: 142 },
+  { id: 'bangles', label: 'Bangles', emoji: '👑', image: 'https://trendia.co/cdn/shop/files/55027835_3.jpg?v=1780295382', description: 'Plain, stone & designer bangles', count: 204 },
+  { id: 'bracelets', label: 'Bracelets', emoji: '📿', image: 'https://assets.ajio.com/medias/sys_master/root/20231004/a1sO/651cc705afa4cf41f5215c09/-473Wx593H-466663725-gold-MODEL.jpg', description: 'Tennis, chain & cuff bracelets', count: 98 },
+  { id: 'chains', label: 'Chains', emoji: '🔗', image: 'https://rudrakshartjewellery.in/cdn/shop/products/IMG_1239_2800x.jpg?v=1754640443', description: 'Singapore, rope & box chains', count: 156 },
+  { id: 'mangalsutra', label: 'Mangalsutra', emoji: '✨', image: 'https://m.media-amazon.com/images/I/61dIJjaZHoL._AC_UY300_.jpg', description: 'Traditional & modern styles', count: 89 },
+  { id: 'nosepins', label: 'Nose Pin', emoji: '👃', image: 'https://shop.southindiajewels.com/wp-content/uploads/2022/06/Taditional-Design-Nose-Pin-1-scaled.jpg', description: 'Classic & diamond nose pins', count: 74 },
+  { id: 'bridal', label: 'Bridal', emoji: '👰', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTlkj32cWHHomMJD-NpuCgyps5ux_WwXijyneI7UiAXi6beNUV_BG7Hg&s=10', count: 168 },
+  { id: 'diamond', label: 'Diamond', emoji: '💖', image: 'https://cdn.orra.co.in/media/catalog/product/cache/10238651d5f95594b9023f998383bb67/o/s/osn21062_ose21062.jpg', count: 234 },
+  { id: 'platinum', label: 'Platinum', emoji: '⚜️', image: 'https://images.meesho.com/images/products/454524369/1dmnu_512.webp?width=512', description: 'Timeless platinum jewellery', count: 45 },
 ];
 
 export const COLLECTIONS = [
